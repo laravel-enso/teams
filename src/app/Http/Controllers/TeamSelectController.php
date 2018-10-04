2b8d9a7190dd961070b23d3e5c2aab02c238feab
mishu <mishu@xtelecom.ro>
2018-10-04 09:06:59 +0300

    fixes import in select controller

--- src/app/Http/Controllers/TeamSelectController.php
+++ src/app/Http/Controllers/TeamSelectController.php
@@ -3,6 +3,7 @@
 namespace LaravelEnso\Core\app\Http\Controllers\Administration\Team;
 
 use Illuminate\Routing\Controller;
+use LaravelEnso\Teams\app\Models\Team;
 use LaravelEnso\Select\app\Traits\OptionsBuilder;
 
 class TeamSelectController extends Controller
