ec5cb82449c8630d0b203feefde4d35a0ca69c1f
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:44:21 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Teams\Http\Controllers\Destroy;
+use LaravelEnso\Teams\Http\Controllers\Index;
+use LaravelEnso\Teams\Http\Controllers\Options;
+use LaravelEnso\Teams\Http\Controllers\Store;
 
-Route::namespace('LaravelEnso\Teams\Http\Controllers')
-    ->middleware(['api', 'auth', 'core'])
+Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/administration/teams')
     ->as('administration.teams.')
     ->group(function () {
-        Route::get('', 'Index')->name('index');
-        Route::post('', 'Store')->name('store');
-        Route::delete('{team}', 'Destroy')->name('destroy');
-        Route::get('options', 'Options')->name('options');
+        Route::get('', Index::class)->name('index');
+        Route::post('', Store::class)->name('store');
+        Route::delete('{team}', Destroy::class)->name('destroy');
+        Route::get('options', Options::class)->name('options');
     });
